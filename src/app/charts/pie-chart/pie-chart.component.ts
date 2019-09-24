8415225033db41f64c53ea4ecaf31f5ae65fd38d
Ziham Zuhair <zihambaba18@gmail.com>
2019-09-24 21:45:43 +0530

    create pie chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,17 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  public pieChartData: number[] = [340, 230, 540];
+  public pieChartLabels: string[] = ['XYZ Logistics','Paan Paan', 'Sumanadisi'];
+  colors: any[] =
+    [
+      {
+        backgroundColor: ['#26547c','#ff6b6b','#ffd166'],
+        borderColor: '#111'
+      }      
+    ];
+    pieChartType = 'doughnut';
+    // pieChartType = 'doughnut';
   ngOnInit() {
   }
 
